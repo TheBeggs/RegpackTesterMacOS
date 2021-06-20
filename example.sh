30ff9f94cc649598197f8fa53a047ac2f02912d5
Chenyu Z <goodzhy@gmail.com>
2021-06-20 23:56:27 +0000

    small matrix run on c5n

--- example.sh
+++ example.sh
@@ -1,3 +1,5 @@
-./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats_small" -t pyfr -n 1
+./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats_small" -t pyfr
+
+# ./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats_small" -t pyfr -n 1
 
 # ./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats_small" -t pyfr -n 1 -s "20-06_13:32:25"
