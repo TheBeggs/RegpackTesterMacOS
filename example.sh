bbd08ac50260b9376ae82bdecf5de3c13fc7dd2d
C Zhang <chenyu.zhang16@imperial.ac.uk>
2021-06-30 16:07:43 +0100

    debug roofline_plot

--- example.sh
+++ example.sh
@@ -1,1 +1,3 @@
-./scripts/benchmark.sh -o log -p plots -m "data/example_mats_small" -t pyfr -n 1
+# ./scripts/benchmark.sh -o log -p plots -m "data/example_mats_small" -t pyfr -n 1
+
+./scripts/benchmark.sh -o log -p plots -m "data/example_mats_small" -t pyfr -n 1 -s 13:40:27
