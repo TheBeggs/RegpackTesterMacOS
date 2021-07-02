ef487a692004220f6e67b82001e619274ff6e4ee
C Zhang <chenyu.zhang16@imperial.ac.uk>
2021-07-02 21:41:54 +0100

    minor change

--- example.sh
+++ example.sh
@@ -1,7 +1,7 @@
-# ./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats" -t pyfr -i 10
+./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats" -t pyfr -i 3 -n 20
 
 # ./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats_small" -t pyfr -n 3 -s "02-07_17:11:53"
 
 # ./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats" -t pyfr -n 3 -s "30-06_15:20:37"
 
-./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats" -t pyfr -i 10 -s "02-07_19:02:47"
+# ./scripts/benchmark_xsmm_only.sh -o log -p plots -m "data/example_mats" -t pyfr -i 10 -s "02-07_19:02:47"
