43065e551190bb2bc805a5b64a4bc653058fb1e5
ChenyuZhang16 <goodzhcy@gmail.com>
2021-07-24 19:59:34 +0000

    add m5n.xlarge info

--- src/plot/cpu_stats.py
+++ src/plot/cpu_stats.py
@@ -19,6 +19,14 @@ xeon_8124M_stats["peak_flops_dp"] = xeon_8124M_stats["avx512_freq"] * xeon_8124M
 xeon_8124M_stats["peak_memory_bw"] = 12.93656 # GB/s measured on c5n.large
 xeon_8124M_stats["linpack_flops_dp"] = 85.1125
 
+xeon_8259CL_stats = dict()
+xeon_8259CL_stats["num_avx512_units"] = 2
+xeon_8259CL_stats["avx512_freq"] = 2.5 # 1 Cores Active AVX512 Boost (GHz), measured 
+xeon_8259CL_stats["peak_flops_dp"] = xeon_8259CL_stats["avx512_freq"] * xeon_8259CL_stats["num_avx512_units"] * AVX512_FLOPS_PER_CYCLE_DP
+xeon_8259CL_stats["peak_memory_bw"] = 12.73666 # GB/s measured on m5n.large
+xeon_8259CL_stats["linpack_flops_dp"] = 70.5427
+
 cpu_stats_dict = dict()
 cpu_stats_dict["Intel(R) Xeon(R) Platinum 8124M CPU @ 3.00GHz"] = xeon_8124M_stats
+cpu_stats_dict["Intel(R) Xeon(R) Platinum 8259CL CPU @ 2.50GHz"] = xeon_8259CL_stats
 cpu_stats_dict["AMD Ryzen 7 3700X 8-Core Processor"] = xeon_8124M_stats # only for testing
